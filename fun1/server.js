08791f057c286a10c4caf27fac1e1a219aea7851
Rahul Rawat <rahulrawat4060@gmail.com>
2025-06-08 23:36:43 +0530

    docker ignore

--- fun1/server.js
+++ fun1/server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-// import {authRouter} from './routes/authRoutes.js';
+import { authRouter } from './routes/authRoutes.js';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -10,7 +10,7 @@ app.use(cors());
 app.get('/', (req, res) => {
     res.send('Hello from the server side!');
 });
-// app.use('/api/auth', authRouter);
+app.use('/api/auth', authRouter);
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
