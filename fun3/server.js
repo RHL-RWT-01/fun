51045c0732f316d842be5c3417d03c971183a3af
Rahul Rawat <rahulrawat4060@gmail.com>
2025-06-11 22:34:29 +0530

    created 3 instances (servers)

--- fun3/server.js
+++ fun3/server.js
@@ -11,13 +11,15 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+const appName = process.env.APP_NAME
+
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
-    console.log("Home page accessed");
+    console.log("Home page accessedfrom: ",appName);
 })
 
 app.listen(port, () => {
-    console.log(`server is listening on port ${port}`);
+    console.log(`server is listening on port ${port} and app name is ${appName}`);
 })
