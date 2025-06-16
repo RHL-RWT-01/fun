f37ef70e217ee5cb81a62d0c8b9516a9819bf203
Rahul Rawat <rahulrawat4060@gmail.com>
2025-06-16 16:57:10 +0530

    kafka setup

--- websockets/server/server.js
+++ websockets/server/server.js
@@ -1,5 +1,6 @@
 import { WebSocketServer } from 'ws';
 import { v4 as uuidv4 } from 'uuid';
+import { Kafka } from 'kafkajs';
 
 const PORT = 8080;
 const wss = new WebSocketServer({ port: PORT });
@@ -16,21 +17,90 @@ wss.on('connection', (ws) => {
   });
 });
 
-setInterval(() => {
-  const transaction = {
-    id: uuidv4(),
-    user: `user_${Math.floor(Math.random() * 1000)}`,
-    amount: (Math.random() * 1000).toFixed(2),
-    timestamp: new Date().toISOString(),
-  };
 
-  const message = JSON.stringify({ type: 'transaction', data: transaction });
+//kafka setup
+const kafka = new Kafka({
+  clientId: 'my-app',
+  brokers: ['kafka:9092']
+})
 
-  for (const client of clients) {
-    if (client.readyState === 1) { 
-      client.send(message);
-    }
-  }
-}, 10000);
+
+const consumer = kafka.consumer({ groupId: 'transaction-group' });
+
+async function kafkaConsumer() {
+  await consumer.connect();
+  await consumer.subscribe({ topic: 'transactions', fromBeginning: true });
+  await consumer.run({
+    eachMessage: async ({ topic, partition, message }) => {
+      const transaction = JSON.parse(message.value.toString());
+      console.log(`Received transaction: ${JSON.stringify(transaction)}`);
+
+      const wsMessage = JSON.stringify({ type: 'transaction', data: transaction });
+
+      for (const client of clients) {
+        if (client.readyState === 1) {
+          client.send(wsMessage);
+        }
+      }
+    },
+  });
+}
+
+
+kafkaConsumer().catch(console.error);
+
+
+
+
+// kafka producer setup
+const producer = kafka.producer();
+
+async function kafkaProducer() {
+  await producer.connect();
+  console.log("Kafka producer connected");
+
+  // Example of sending a transaction
+  setInterval(async () => {
+    const transaction = {
+      id: uuidv4(),
+      user: `user_${Math.floor(Math.random() * 1000)}`,
+      amount: (Math.random() * 1000).toFixed(2),
+      timestamp: new Date().toISOString(),
+    };
+    const message = {
+      value: JSON.stringify(transaction),
+    };
+    await producer.send({
+      topic: 'transactions',
+      messages: [message],
+    });
+    console.log(`Sent transaction: ${JSON.stringify(transaction)}`);
+  },5000);
+}
+
+
+
+
+
+
+
 
 console.log(`WebSocket server running at ws://localhost:${PORT}`);
+
+// setInterval(() => {
+//   const transaction = {
+//     id: uuidv4(),
+//     user: `user_${Math.floor(Math.random() * 1000)}`,
+//     amount: (Math.random() * 1000).toFixed(2),
+//     timestamp: new Date().toISOString(),
+//   };
+
+//   const message = JSON.stringify({ type: 'transaction', data: transaction });
+
+//   for (const client of clients) {
+//     if (client.readyState === 1) {
+//       client.send(message);
+//     }
+//   }
+// }, 10000);
+
