2907c257a5f8712c0d8d9db4c33fcb9a04c9977b
Rahul Rawat <rahulrawat4060@gmail.com>
2025-06-29 09:02:46 +0530

    route todelet user with id

--- fun4/server.js
+++ fun4/server.js
@@ -6,20 +6,26 @@ const port = 3000;
 
 app.use(express.json());
 
-const users=[];
+const users = [];
 
 app.get('/', (req, res) => {
     res.status(500).json({ msg: "hi" });
 })
 
 app.post('/users', async (req, res) => {
-    const {name,email,id}=req.body;
-    users.push({name,email,id});
-    res.status(200).json({msg:"User added success"});
+    const { name, email, id } = req.body;
+    users.push({ name, email, id });
+    res.status(200).json({ msg: "User added success" });
 })
 
-app.get('/users',(req,res)=>{
+app.get('/users', (req, res) => {
     res.send(users);
+})
+
+app.delete('/users/:id',(req,res)=>{
+    const {id}=req.params;
+    users.filter((user)=> id!==user.id)
+    res.send(`user with id ${id} deleted`);
 })
 
 app.listen(port, () => {
