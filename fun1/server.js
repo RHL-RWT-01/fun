299cada902b393f4f5cf58e620b436526ca1f079
Rahul Rawat <rahulrawat4060@gmail.com>
2025-06-30 20:45:09 +0530

    Update server.js

--- fun1/server.js
+++ fun1/server.js
@@ -6,7 +6,10 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
-
+t
+app.use((req, res, next) => {
+  res.status(404).json({ error: 'Route not found' });
+});
 app.get('/', (req, res) => {
     res.send('Hello from the server side!');
 });
